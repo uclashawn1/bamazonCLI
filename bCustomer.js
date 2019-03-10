794212b5c05681d8ab48600bff1db00f49e272cb
UCLAShawn3G <uclashawn1@gmail.com>
2019-03-10 01:07:59 -0800

    first commit. basic structure

---
+++ bCustomer.js
@@ -0,0 +1,26 @@
+//require variables
+
+
+
+//mySQL connection
+
+
+// Display products database using a table made with the npm package cli-table2
+
+// then Prompt the user to determine item and quantity they want to purchase
+
+// Prompt user to enter id and quantity they wish to purchase
+
+// Query the database for info about the item including the quantity currently in stock.
+
+//If enough inventory to complete order, process order by updating database inventory and notifying customer that order is complete.
+               
+// Order completed
+               
+//If not enough inventory notify customer and prompt customer for desire to shop more
+
+// Order not completed
+                
+
+// Start app by Prompting the customer
+customerPrompt();
---
+++ bamazonDB.sql
@@ -0,0 +1,8 @@
+-- Create the "bamazon" database --
+
+CREATE DATABASE bamazon;
+
+-- Use database --
+USE bamazon;
+
+-- Creates product table --
