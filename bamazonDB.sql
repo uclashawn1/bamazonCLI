5e43384cb9a3c80fa1d89740c7b8e09f0fb11189
UCLAShawn3G <uclashawn1@gmail.com>
2019-03-10 01:34:46 -0800

    sql database created in mysql. content copied here.

--- bamazonDB.sql
+++ bamazonDB.sql
@@ -1,8 +1,37 @@
--- Create the "bamazon" database --
+DROP DATABASE if EXISTS bamazon;
 
 CREATE DATABASE bamazon;
 
--- Use database --
 USE bamazon;
 
--- Creates product table --
+CREATE TABLE products (
+	ItemID INTEGER(11) AUTO_INCREMENT NOT NULL,
+  ProductName  VARCHAR(50) NOT NULL,
+  DepartmentName VARCHAR(50) NOT NULL,
+  Price DECIMAL(10,2),
+	StockQuantity INTEGER(10),
+  PRIMARY KEY (ItemID)
+);
+
+INSERT INTO products (ProductName,DepartmentName,Price,StockQuantity)
+VALUES ("Tekken", "Games", 24.99, 5);
+INSERT INTO products (ProductName,DepartmentName,Price,StockQuantity)
+VALUES ("Street Fighter", "Games", 10.95, 1);
+INSERT INTO products (ProductName,DepartmentName,Price,StockQuantity)
+VALUES ("Mortal Combat", "Games", 19.99, 23);
+INSERT INTO products (ProductName,DepartmentName,Price,StockQuantity)
+VALUES ("Avengers: Infinity War", "Movies", 24.95, 15);
+INSERT INTO products (ProductName,DepartmentName,Price,StockQuantity)
+VALUES ("Doctor Strange", "Movies", 17.95, 52);
+INSERT INTO products (ProductName,DepartmentName,Price,StockQuantity)
+VALUES ("Guardians of the Galaxy 2", "Movies", 19.99, 12);
+INSERT INTO products (ProductName,DepartmentName,Price,StockQuantity)
+VALUES ("Thor: Ragnarok", "Movies", 21.50, 10);
+INSERT INTO products ( ProductName,DepartmentName,Price,StockQuantity)
+VALUES ("Lenovo Flex", "Computers", 1295.99, 3);
+INSERT INTO products (ProductName,DepartmentName,Price,StockQuantity)
+VALUES ("HP", "Computers", 669.99, 7);
+INSERT INTO products (ProductName,DepartmentName,Price,StockQuantity)
+VALUES ("Dell", "Computers", 599.99, 3);
+
+select * FROM products
